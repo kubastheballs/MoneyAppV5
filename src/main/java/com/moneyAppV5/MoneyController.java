ed162a0e2ba15d88d9a97708a85f2b99d2b91c15
Jakub <kubastheballs@outlook.com>
2022-02-25 06:15:49 +0100

    addeed categorie.html transactions.html and MoneyController

---
+++ src/main/java/com/moneyAppV5/MoneyController.java
@@ -0,0 +1,16 @@
+package com.moneyAppV5;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/money")
+class MoneyController
+{
+    @GetMapping
+    String showMoneyApp()
+    {
+        return "money";
+    }
+}
